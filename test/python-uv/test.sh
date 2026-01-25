02b0f5a5c28cc510bee6f082086cc4ef83e32516
Metin Senturk <metinsenturk@me.com>
2026-01-25 00:26:50 +0000

    fix the tests

--- test/python-uv/test.sh
+++ test/python-uv/test.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 cd $(dirname "$0")
-source ../test-utils/test-utils.sh
+source test-utils.sh
 
 # Template specific tests
-check "python version" python --version
-check "pip installed" pip --version
 check "git installed" git --version
 
 # uv checks (requires uv to be installed in the container)
 check "uv installed" uv --version
 
+# installed by uv tests
+check "python not on path" sh -c '! command -v python'
+check "python version" uv run python --version
+
 # Basic Python functionality
-check "python basic functionality" python -c "print('Hello from Python+uv test')"
+check "python basic functionality" uv run python -c "print('Hello from Python+uv test')"
 
 # Report result
 reportResults
